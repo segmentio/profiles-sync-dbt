926eabfabd26fdaa2d1d3b0c046ec86ed0b10377
aawang@twilio.com <aawang@twilio.com>
2023-06-05 19:26:44 +0000

    new profile_traits_updates materialization

---
+++ macros/last_observed_trait_v2.sql
@@ -0,0 +1,31 @@
+{# /* postgres (and older versions of Synapse) lack "LAST VALUE" functionality - need a workaround*/ #}
+
+
+{% macro last_observed_profile_trait(col) %}
+  {{ return(adapter.dispatch('last_observed_profile_trait')(col)) }}
+{% endmacro %}
+
+
+{% macro default__last_observed_profile_trait(col) %}
+    LAST_VALUE(updates.{{ col }} IGNORE NULLS) 
+        OVER(PARTITION BY COALESCE(id_graph.canonical_segment_id,updates.segment_id) ORDER BY updates.seq 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS {{ col }}
+{% endmacro %}
+
+{% macro redshift__last_observed_profile_trait(col) %}
+    LAST_VALUE(updates.{{ col }} IGNORE NULLS) 
+        OVER(PARTITION BY COALESCE(id_graph.canonical_segment_id,updates.segment_id) ORDER BY updates.seq 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS {{ col }}
+{% endmacro %}
+
+
+{% macro synapse__last_observed_profile_trait(col) %}
+    LAST_VALUE(updates.{{ col }} ) IGNORE NULLS
+        OVER(PARTITION BY COALESCE(id_graph.canonical_segment_id,updates.segment_id) ORDER BY updates.seq ) AS {{ col }}
+{% endmacro %}
+
+{% macro postgres__last_observed_profile_trait(col) %}
+    find_last_ignore_nulls(updates.{{ col }}) 
+        OVER(PARTITION BY COALESCE(id_graph.canonical_segment_id,updates.segment_id) ORDER BY updates.seq 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS {{ col }}
+{% endmacro %}
---
+++ models/profile_materializations/profile_traits_v2_incrementaltest.sql
@@ -0,0 +1,86 @@
+{{ config(unique_key='canonical_segment_id') }}
+
+{# Define and execute query for col names (traits) in identifies table #}
+{%- set get_trait_columns %}
+    SELECT 
+        column_name
+    FROM {{ col_table(var("schema_name")) }} 
+    WHERE LOWER(table_schema) = LOWER('{{ var("schema_name") }}')
+        AND LOWER(table_name) = 'identifies'
+        --AND LOWER(table_name) = 'profile_traits_updates'
+        AND NOT LOWER(column_name) IN ('user_id','anonymous_id','id','canonical_segment_id','merged_to','sent_at','received_at')
+        AND NOT LOWER(column_name) LIKE  'context_%'
+        AND LEFT(column_name,1) <> '_'
+    ORDER BY 1
+{% endset -%}
+{% set results = run_query(get_trait_columns) %}
+{%- if execute %}
+    {% set column_names = results.columns[0].values() %}
+{% else %}
+    {% set column_names = [] %}
+    {% set ts = [] %}
+{% endif -%}
+
+
+    
+-- - - IIa. Build profiles (incremental) - - - - - - - - 
+-- Outer join between:
+-- 1) List of merged-away + newly-observed profiles
+--     (Add tombstone for merged-away profiles - the segment_id that it merged into)
+-- 2) List of profile updates (if an identify came in on a profile that was later merged, that is also accounted for)
+-- 
+-- Left join back to original table to ensure we don't overwrite any traits with NULL
+-- - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+{# Define and execute query for (a) col names (traits) in existing profile_traits table and (b) latest landing time of an event #}
+{%- set existing_cols_orig = adapter.get_columns_in_relation(this) -%}
+{% set existing_cols = [] %}
+{%- for col in existing_cols_orig  %}
+{{- existing_cols.append( col.name ) or "" -}} 
+{% endfor -%}
+
+{%- set get_max_ts %} SELECT CAST(MAX(uuid_ts) AS {{datetime_univ()}}) FROM {{ this }} {% endset -%}
+{% set results2 = run_query(get_max_ts) %}
+{%- if execute %}
+    {% set ts = results2.columns[0].values()[0] %}
+{% else %}
+    {% set ts = [] %}
+{% endif -%}
+
+
+
+WITH id_graph AS (
+    SELECT * FROM {{ ref('id_graph') }} 
+    WHERE CAST(etl_ts as {{datetime_univ()}}) >= {{ dateadd2('hour', -var('etl_overlap'), '\'' ~ ts ~ '\'') }}
+),
+
+
+updates as (
+    SELECT 
+        COALESCE(id_graph.canonical_segment_id,updates.segment_id) as canonical_segment_id,
+        {% for col in column_names %}
+            {{ last_observed_profile_trait(col) }},
+            {% endfor %}
+        row_number() OVER(PARTITION BY COALESCE(id_graph.canonical_segment_id,updates.segment_id)
+            ORDER BY CASE WHEN updates.seq IS NULL THEN '0' ELSE updates.seq END DESC) AS rn
+    FROM {{ var("schema_name") }}.identifies AS updates
+    --FROM {{ var("schema_name") }}.profile_traits_updates AS updates
+    LEFT JOIN id_graph
+        ON id_graph.segment_id = updates.segment_id
+    WHERE CAST(updates.uuid_ts as {{datetime_univ()}})  > (SELECT MAX(timestamp) FROM {{ this }})
+)
+
+
+SELECT 
+    updates.canonical_segment_id as canonical_segment_id,
+    {% for col in column_names %}
+          {%- if col in existing_cols %}
+             COALESCE(updates.{{col}}, orig.{{ col }}) AS {{ col }},
+          {%- else %}
+             updates.{{col}} AS {{ col }},
+          {% endif %}
+        {%- if not loop.last %},{% endif %}
+        {% endfor %}
+FROM updates
+where rn = 1
